085e97c4b35fd6fd1b3025d0af8b198dec93ae7f
D German <dmg@uvic.ca>
2016-09-02 15:44:36 -0700

    updated tester script

--- tutorials/01_hello_world/files/part3/tester.cmake
+++ tutorials/01_hello_world/files/part3/tester.cmake
@@ -11,12 +11,12 @@ if( run_not_successful)
   message( SEND_ERROR "Test ${ID} could not be executed")
 else( run_not_successful)
   execute_process( 
-    COMMAND ${CMAKE_COMMAND} -E compare_files ../tests/test-${ID}.out test-${ID}.out
+    COMMAND ${CMAKE_COMMAND} -E compare_files ../tests/test-${ID}.expected test-${ID}.out
     RESULT_VARIABLE test_not_successful
     )
 endif( run_not_successful )
 
 
 if( test_not_successful)
-   message( SEND_ERROR "../tests/test-${ID}.out does not match test-${ID}.out")
+   message( SEND_ERROR "../tests/test-${ID}.expected does not match test-${ID}.out")
 endif( test_not_successful )
